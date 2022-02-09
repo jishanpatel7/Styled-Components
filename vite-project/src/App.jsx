519a73f18afaeb5072f175bed4f57ae6f4eb08b5
jishan <jishanpatel78@gmail.com>
2022-02-09 15:33:37 +0530

    commit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,14 +1,34 @@
-import {Container} from './components/container-style.jsx';
-import {Header} from './components/header.jsx';
+import "./App.css"
+import { Header } from "./components/header";
 function App() {
- 
+  const data = [
+    {rank:1, name: "Domenic", points: "88,110", team: "dcode" },
+    { rank:2,name: "Sally", points: "72,400", team: "Students" },
+    {rank:3, name: "Nick", points: "52,300", team: "dcode"},
+  ]
   return (
-<>
-<Header />
-<Container>
-    <h1>Hello World</h1>
-</Container>
-</>
+    <div className="App">
+    <table>
+        <thead>
+        <th>Rank</th>
+        <th>Name</th>
+        <th>Points</th>
+        <th>Team</th>
+        </thead>
+ 
+      {data.map((val, key) => {
+        return (
+          <tr key={key}>
+            <td>{val.rank}</td>
+            <td>{val.name}</td>
+            <td>{val.points}</td>
+            <td>{val.team}</td>
+          </tr>
+        )
+      })}
+    </table>
+ 
+  </div>
   )
 }
 
--- vite-project/src/components/header-style.jsx
+++ vite-project/src/components/header-style.jsx
@@ -1,14 +1,8 @@
 import styled from "styled-components";
 
-export const StyleHeader = styled.header`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 20px 0;
-    background: #ebfbff;
-    border-bottom: 1px solid #eee;
-
-    h1 {
-        font-size: 24px;
-    }
+export const StyleHeader = styled.table`
+    border: 2px solid forestgreen;
+    width: 800px;
+    height: 200px;
+    border-radius:10px;
     `;
